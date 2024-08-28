1b8982aa035266cc788658d40cb1e0c7e21368d2
riyazd19 <91775182+riyazd19@users.noreply.github.com>
2024-08-28 10:34:28 +0530

    main method

--- testingPractice/src/main/java/abastrationAndInterfaces/MainMethod.java
+++ testingPractice/src/main/java/abastrationAndInterfaces/MainMethod.java
@@ -2,7 +2,17 @@ package abastrationAndInterfaces;
 
 public class MainMethod {
     public static void main(String[] args) {
-        ClassOne obj1 = new ClassOne();
-        obj1.makeNoise();
+        String name ="abcd";
+       // ClassOne obj1 = new ClassOne();
+       // obj1.makeNoise();
+       System.out.println(name);
+        fun2(name);
+
+    }
+    public static void fun2(String Name){
+        
+       String name="1232";
+        System.out.println(" "+name);
+
     }
 }
