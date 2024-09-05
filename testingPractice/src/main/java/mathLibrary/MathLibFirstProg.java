cf32b99cc8d05e95e0e3019c11b1829b4427c98d
riyazd19 <91775182+riyazd19@users.noreply.github.com>
2024-09-05 11:39:55 +0530

    math length vs length

---
+++ testingPractice/src/main/java/mathLibrary/MathLibFirstProg.java
@@ -0,0 +1,11 @@
+package mathLibrary;
+
+public class MathLibFirstProg {
+    static int x=4,y=55;
+    public static void main(String[] args) {
+        double z=22.3432;
+        System.out.println("   "+Math.max(x, y));
+        System.out.println(Math.abs(z));
+        System.out.println("  "+Math.sin(90));
+    }
+}
---
+++ testingPractice/src/main/java/stringPackage/LengthVlength.java
@@ -0,0 +1,11 @@
+package stringPackage;
+
+public class LengthVlength {
+    public static void main(String[] args) {
+        String s="abcdefghijkl";
+        System.out.println("   ");
+        System.out.println(s.length());
+        char[] a= s.toCharArray();
+        System.out.println(a.length);
+    }
+}
